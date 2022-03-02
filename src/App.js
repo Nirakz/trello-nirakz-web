664e1590b4e383de634fdd6d60fdfcfba6df867c
nirakz <ntchinh271099@gmail.com>
2022-03-02 16:30:41 +0700

    Design trello Fontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,103 @@
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hi Chinh</h1>
+    <div className="trello-nirakz-master">
+     <nav className='navbar app'>App Bar</nav>
+     <nav className='navbar board'>Board Bar</nav>
+     <div className='board-columns'>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+           <img src="https://toigingiuvedep.vn/wp-content/uploads/2021/01/hinh-anh-cute-de-thuong.jpg" alt="nirakz-alt-img" ></img>
+          Title: nirakz
+          </li>
+          <li>Add what</li>
+          <li>Add what</li>
+          <li>Add what</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+           <img src="https://toigingiuvedep.vn/wp-content/uploads/2021/01/hinh-anh-cute-de-thuong.jpg" alt="nirakz-alt-img" ></img>
+          Title: nirakz
+          </li>
+          <li>Add what</li>
+          <li>Add what</li>
+          <li>Add what</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+           <img src="https://toigingiuvedep.vn/wp-content/uploads/2021/01/hinh-anh-cute-de-thuong.jpg" alt="nirakz-alt-img" ></img>
+          Title: nirakz
+          </li>
+          <li>Add what</li>
+          <li>Add what</li>
+          <li>Add what</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+           <img src="https://toigingiuvedep.vn/wp-content/uploads/2021/01/hinh-anh-cute-de-thuong.jpg" alt="nirakz-alt-img" ></img>
+          Title: nirakz
+          </li>
+          <li>Add what</li>
+          <li>Add what</li>
+          <li>Add what</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+           <img src="https://toigingiuvedep.vn/wp-content/uploads/2021/01/hinh-anh-cute-de-thuong.jpg" alt="nirakz-alt-img" ></img>
+          Title: nirakz
+          </li>
+          <li>Add what</li>
+          <li>Add what</li>
+          <li>Add what</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+           <img src="https://toigingiuvedep.vn/wp-content/uploads/2021/01/hinh-anh-cute-de-thuong.jpg" alt="nirakz-alt-img" ></img>
+          Title: nirakz
+          </li>
+          <li>Add what</li>
+          <li>Add what</li>
+          <li>Add what</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+           <img src="https://toigingiuvedep.vn/wp-content/uploads/2021/01/hinh-anh-cute-de-thuong.jpg" alt="nirakz-alt-img" ></img>
+          Title: nirakz
+          </li>
+          <li>Add what</li>
+          <li>Add what</li>
+          <li>Add what</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+     </div>
     </div>
   );
 }
